6a99dbf3473e4d6696753df444a3b891f74ad32d
wkdejr16 <wkdejr16@naver.com>
2015-07-31 16:38:09 +0900

    Create Check_Nums.rb

---
+++ Check_Nums.rb
@@ -0,0 +1,13 @@
+def CheckNums(num1,num2)
+
+  # code goes here
+  if num2 == num1
+    return '-1'
+  else
+    bool= num1>num2 ? false : true
+    return bool
+  end
+  return num1 
+         
+end
+   
